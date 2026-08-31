51ed0821b5e408265a271f9797e585c8b52a1291
rohit.kadam2 <rohit.kadam2@cipla.com>
2026-08-31 14:39:34 +0530

    created vite+react project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World2</h1>
+    </div>
+  )
+}
+
+export default App
